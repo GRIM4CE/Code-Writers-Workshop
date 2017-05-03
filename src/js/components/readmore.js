b9197a9a34cfedfc1ef09780774c9418a0b14f29
Jon Leibham <jleibham@gmail.com>
2017-05-03 08:58:11 -0500

    schedule edits

--- src/js/components/readmore.js
+++ src/js/components/readmore.js
@@ -5,21 +5,26 @@ class ReadMore {
     this.readMoreText = this.panel.querySelector('.read-more-text');
     this.bindReadMoreButton();
   }
+
   toggleReadMore() {
-    if (this.readMoreText.classList.contains('closed')) {
-      this.toggleState(this.readMoreText, 'open', 'closed');
-      this.readMoreButton.innerText = 'Read Less';
-    } else {
-      this.toggleState(this.readMoreText, 'open', 'closed');
-      this.readMoreButton.innerText = 'Read More';
-    }
+    this.toggleState(this.readMoreText, 'open', 'closed');
+    this.toggleText(this.readMoreButton.innerText, 'Read More', 'Read Less');
   }
+
   bindReadMoreButton() {
     this.readMoreButton.addEventListener('click', this.toggleReadMore.bind(this));
   }
 
   toggleState(elem, one, two) {
     elem.setAttribute('data-state', elem.getAttribute('data-state') === one ? two : one);
+  }
+
+  toggleText(elem, one, two) {
+    if(elem === one) {
+      this.readMoreButton.innerText = two
+    } else {
+      this.readMoreButton.innerText = one;
+    }
   }
 }
 
